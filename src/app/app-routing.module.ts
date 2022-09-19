cc77f73119d87c53d222169772302a254bc4fce0
web dev <rublin@gmx.de>
2022-09-19 17:52:28 +0200

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "tabs", loadChildren: "./tabs/tabs.module#TabsPageModule" },
+  { path: "login", loadChildren: "./login/login.module#LoginPageModule" },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
